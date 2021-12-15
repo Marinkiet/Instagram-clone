b86931ed71eee91814596b4b834c0eb6257f202d
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2021-12-15 03:55:31 +0200

    header and post components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Post from './Post';
 function App() {
+
+  const [state, setstate] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Header*/}
+      <div className="app_header">
+      <img className="app_headerImage" src="logo.jpg"
+      alt="logo"></img></div>
+    
+      {/*post*/}
+      <Post
+      username="MarinkieJ"
+      caption=" Wisper Memoji"
+      imageurl="post (1).png"
+      
+      />
+      <Post
+      imageurl="post (2).png"
+      caption=" Coder all day Memoji"
+      username="MarinkieM"/>
+      <Post
+      imageurl="post (3).png"
+      caption=" Hello Memoji"
+      username="MarinkieK"/>
+      <Post
+      imageurl="post (4).png"
+      caption=" Hi Memoji"
+      username="MarinkieL"/>
+      <Post
+      imageurl="post (5).png"
+      caption=" Call Me Memoji"
+      username="MarinkieR"/>
+      <Post
+      imageurl="post (6).png"
+      caption=" Sick Memoji"
+      username="MarinkieS"/>
+      {/*post*/}
+      {/*post*/}
     </div>
   );
 }
 
 export default App;
+
+//timestamp 50:30
---
+++ src/Post.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
+function Post({username,caption,imageurl}) {
+    return (
+        <div className="post">
+             {/*header->avatar->username */}
+            <div className='post_Header'>
+                <Avatar className="post_Avatar"
+                alt="MarinkieT"
+                src={imageurl}/>
+                 <h3>{username}</h3>
+            </div>
+            {/*image*/}
+            <img className ="post_Image" src={imageurl} ></img>
+            {/*username + caption*/}
+            <h4 className='post_Text'><strong>{username}</strong>{caption}</h4>
+        </div>
+    )
+}
+
+export default Post
